f793cd57491ae64f9d1f6e42835d18e97998cdd2
MCXCC <1270116382@qq.com>
2024-08-18 16:55:49 +0800

    ```txtfeat(department): 添加记录创建者信息
    在添加部门信息时，现在会记录创建者的信息。通过从请求中提取用户ID，并将其分配给部门实体的创建者字段，以增强审计跟踪的可追溯性。
    ```

--- src/main/java/org/example/clztoolsconsole/sys/department/web/SysDepartmentController.java
+++ src/main/java/org/example/clztoolsconsole/sys/department/web/SysDepartmentController.java
@@ -1,9 +1,11 @@
 package org.example.clztoolsconsole.sys.department.web;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.example.clztoolsconsole.sys.department.entity.SysDepartment;
 import org.example.clztoolsconsole.sys.department.service.SysDepartmentService;
+import org.example.clztoolsconsole.sys.user.entity.SysUser;
 import org.example.clztoolsconsole.utils.AjaxJson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +27,10 @@ public class SysDepartmentController {
     }
 
     @PostMapping("/add")
-    public AjaxJson addDepartment(@RequestBody SysDepartment sysDepartment) {
+    public AjaxJson addDepartment(@RequestBody SysDepartment sysDepartment,HttpServletRequest request) {
+        SysUser sysUser = new SysUser();
+        sysUser.setId(Integer.parseInt( request.getAttribute("uid").toString()));
+        sysDepartment.setUpdatedBy(sysUser);
         sysDepartmentService.addDepartment(sysDepartment);
         return AjaxJson.success("操作成功");
     }
