1fd58ff718201037c85076a95dd3bae3a71d545a
MCXCC <1270116382@qq.com>
2024-09-10 12:01:37 +0800

    新增`@EqualsAndHashCode`注解，强化`SysUser`类比较逻辑
    在`SysUser`类中添加`@EqualsAndHashCode`注解，确保在进行equals和hashCode计算时包括父类`BeanEntity`的字段。这提高了实体比较和集合操作的一致性与准确性。

--- src/main/java/org/example/clztoolsconsole/sys/user/entity/SysUser.java
+++ src/main/java/org/example/clztoolsconsole/sys/user/entity/SysUser.java
@@ -1,11 +1,13 @@
 package org.example.clztoolsconsole.sys.user.entity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.example.clztoolsconsole.sys.department.entity.SysDepartment;
 import org.example.clztoolsconsole.utils.BeanEntity;
 
 import java.io.Serial;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class SysUser extends BeanEntity {
     @Serial
