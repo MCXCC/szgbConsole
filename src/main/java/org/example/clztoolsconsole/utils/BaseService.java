225f91e3c462f8c0e007f6913fb109b20506cf83
chenlingzhi <1270116382@qq.com>
2024-09-19 12:48:21 +0800

    ```删除操作日志记录优化
    删除方法中移除日志记录行，以优化性能和清晰度。此次调整简化了删除流程，不再记录每个被删ID的日志。
    ```

--- src/main/java/org/example/clztoolsconsole/utils/BaseService.java
+++ src/main/java/org/example/clztoolsconsole/utils/BaseService.java
@@ -51,7 +51,6 @@ public abstract class BaseService<M extends BaseMapper<T>, T extends BeanEntity<
         String[] id = ids.split(",");
         for (String s : id) {
             mapper.delete(Integer.parseInt(s));
-            log.info("删除id为：{}的数据", s);
         }
     }
 }
