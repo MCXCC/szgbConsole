2823db562e4b8791fde5a7551940ec71ba9ce832
MCXCC <1270116382@qq.com>
2024-09-20 22:59:42 +0800

    refactor(console): 更新AjaxJson工具并调整控制器以使用新的响应处理
    - 重构AjaxJson工具类，使用Jakarta的Servlet API替代Lombok和Hutool依赖。- 在MaterialController、SysDepartmentController和SysUserController中调整端点以使用更新后的AjaxJson类。
    - 确保所有控制器端点在创建响应时正确处理HttpServletRequest和HttpServletResponse。
    - 在AjaxJson中添加新的静态方法以支持直接的成功和错误响应创建。
    - 在用户登录端点中使用这些新方法处理登录成功和失败的响应。

--- src/main/java/org/example/clztoolsconsole/utils/AjaxJson.java
+++ src/main/java/org/example/clztoolsconsole/utils/AjaxJson.java
@@ -21,6 +21,30 @@ public class AjaxJson extends HashMap<String, Object> implements Serializable {
         this.put("msg", "操作成功");
     }
 
+    public String getMsg() {
+        return (String) this.get("msg");
+    }
+
+    public void setMsg(String msg) {//向json中添加属性，在js中访问，请调用data.msg
+        this.put("msg", msg);
+    }
+
+    public boolean isSuccess() {
+        return (boolean) this.get("success");
+    }
+
+    public void setSuccess(boolean success) {
+        this.put("success", success);
+    }
+
+    public int getCode() {
+        return (int) this.get("code");
+    }
+
+    public void setCode(int code) {
+        this.put("code", code);
+    }
+
     @JsonIgnore
     public static AjaxJson success(String msg, HttpServletRequest request, HttpServletResponse response) {
         AjaxJson j = new AjaxJson();
@@ -50,30 +74,6 @@ public class AjaxJson extends HashMap<String, Object> implements Serializable {
         AjaxJson j = new AjaxJson();
         response.setStatus(j.getCode());
         return j;
-    }
-
-    public String getMsg() {
-        return (String) this.get("msg");
-    }
-
-    public void setMsg(String msg) {//向json中添加属性，在js中访问，请调用data.msg
-        this.put("msg", msg);
-    }
-
-    public boolean isSuccess() {
-        return (boolean) this.get("success");
-    }
-
-    public void setSuccess(boolean success) {
-        this.put("success", success);
-    }
-
-    public int getCode() {
-        return (int) this.get("code");
-    }
-
-    public void setCode(int code) {
-        this.put("code", code);
     }
 
     @Override
