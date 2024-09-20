1ad4e7e6bcaf4d333e1014de19f010e180fb56be
MCXCC <1270116382@qq.com>
2024-09-20 21:51:07 +0800

    新增`BaseService`保存方法及数据版本冲突检查
    在`BaseService`类中新增了一个保存实体对象的方法。该方法会检查实体对象的ID以决定是进行插入操作还是更新操作。此外，该方法还实现了数据版本冲突检查，以防止并发更新时的数据不一致问题。

--- src/main/java/org/example/clztoolsconsole/utils/BaseService.java
+++ src/main/java/org/example/clztoolsconsole/utils/BaseService.java
@@ -35,17 +35,36 @@ public abstract class BaseService<M extends BaseMapper<T>, T extends BeanEntity<
         return mapper.getCount(entity);
     }
 
+    /**
+     * 保存实体对象
+     * 如果实体对象的ID为null，则视为新实体进行插入操作
+     * 如果实体对象的ID不为null，则视为现有实体进行更新操作，但需检查数据版本是否冲突
+     *
+     * @param entity 要保存的实体对象，类型为泛型T
+     * @return 返回一个Map对象，键为Boolean表示操作是否成功，值为String表示操作的结果信息
+     */
     public Map<Boolean,String> save(T entity) {
+        // 检查实体对象的ID是否为null，判断是否为新实体
         if (entity.getId() == null) {
+            // 对于新实体，设置创建者信息
             entity.setCreatedBy(entity.getUpdatedBy());
+            // 执行插入操作
             mapper.insert(entity);
+            // 返回成功插入的信息
             return Map.of(true,"新增成功");
         } else {
+            // 对于现有实体，根据ID获取数据库中的实体对象
             T t = mapper.get(entity);
+            // 检查数据版本是否冲突
             if (!Objects.equals(t.getVersion(), entity.getVersion())){
+                // 如果数据版本冲突，返回错误信息
                 return Map.of(false,"数据版本冲突，请刷新后重试");
             }
+            // 更新实体的数据版本
+            entity.setVersion(entity.getVersion()+1);
+            // 执行更新操作
             mapper.update(entity);
+            // 返回成功更新的信息
             return Map.of(true,"修改成功");
         }
     }
