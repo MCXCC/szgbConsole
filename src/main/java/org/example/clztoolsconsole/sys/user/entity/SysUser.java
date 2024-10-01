5f81c4340e5ddef117559806f3efe8fab7b44044
MCXCC <1270116382@qq.com>
2024-10-01 09:37:35 +0800

    在 SysUser 实体中添加工号字段并更新相关映射- 在 SysUser 类中新增 no 字段，用于存储用户工号 - 在 SysUserMapper.xml 中添加 no 字段的映射和查询条件 - 修复插入语句中参数顺序错误，确保正确分配 UUID - 更新更新语句，包含 no 字段的设置

--- src/main/java/org/example/clztoolsconsole/sys/user/entity/SysUser.java
+++ src/main/java/org/example/clztoolsconsole/sys/user/entity/SysUser.java
@@ -13,6 +13,7 @@ public class SysUser extends BeanEntity<SysUser> {
     @Serial
     private static final long serialVersionUID = 1L;
     private String username;
+    private String no;
     private String name;
     private String password;
     private String token;
