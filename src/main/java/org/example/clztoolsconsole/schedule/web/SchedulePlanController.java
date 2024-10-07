384f62ae745f8f4854cc3eb4294a86c18b46d42a
MCXCC <1270116382@qq.com>
2024-10-07 22:41:56 +0800

    新增字段和控制器以支持排班计划
    在 Schedule 实体类中添加了多个字段来表示白班和夜班的领班、抢修组长、信息联络、物料准备和其他人员。还引入了 List<SchedulePlan> 字段来存储排班计划。
    
    SchedulePlanController 类现在继承自 BaseController，并对其进行了必要的调整以支持 SchedulePlan 实体的 CRUD 操作。

--- src/main/java/org/example/clztoolsconsole/schedule/web/SchedulePlanController.java
+++ src/main/java/org/example/clztoolsconsole/schedule/web/SchedulePlanController.java
@@ -1,7 +1,10 @@
 package org.example.clztoolsconsole.schedule.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.clztoolsconsole.schedule.entity.SchedulePlan;
+import org.example.clztoolsconsole.schedule.mapper.SchedulePlanMapper;
 import org.example.clztoolsconsole.schedule.service.SchedulePlanService;
+import org.example.clztoolsconsole.utils.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping(value = "/schedule")
-public class SchedulePlanController {
+public class SchedulePlanController extends BaseController<SchedulePlanService, SchedulePlanMapper, SchedulePlan> {
     private final SchedulePlanService schedulePlanService;
 
     @Autowired
