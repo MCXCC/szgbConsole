3f5f061afe2c131ae65958679dd3bc7095977610
MCXCC <1270116382@qq.com>
2024-10-08 18:41:53 +0800

    修改SchedulePlanController类的请求映射路径
    将SchedulePlanController类中的@RequestMapping注解的value值从"/schedule"修改为"/schedule/plan"，以便在URL路径中更具体地指定计划相关的调度操作。

--- src/main/java/org/example/clztoolsconsole/schedule/web/SchedulePlanController.java
+++ src/main/java/org/example/clztoolsconsole/schedule/web/SchedulePlanController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping(value = "/schedule")
+@RequestMapping(value = "/schedule/plan")
 public class SchedulePlanController extends BaseController<SchedulePlanService, SchedulePlanMapper, SchedulePlan> {
     private final SchedulePlanService schedulePlanService;
 
