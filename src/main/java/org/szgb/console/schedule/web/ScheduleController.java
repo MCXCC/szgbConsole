a0449644fe57d133a9fe8c47bc1e2c09a91d63dc
MCXCC <1270116382@qq.com>
2024-10-29 21:17:58 +0800

    refactor(schedule): 优化排班表导出功能
    - 将导出接口从 GET 方法改为 POST 方法
    - 添加请求体参数以支持更多自定义导出选项- 优化了导出数据的获取逻辑，提高代码复用性

--- src/main/java/org/szgb/console/schedule/web/ScheduleController.java
+++ src/main/java/org/szgb/console/schedule/web/ScheduleController.java
@@ -6,9 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.szgb.console.schedule.entity.Schedule;
 import org.szgb.console.schedule.mapper.ScheduleMapper;
 import org.szgb.console.schedule.service.ScheduleService;
@@ -28,19 +26,19 @@ public class ScheduleController extends BaseController<ScheduleService, Schedule
         super(scheduleService);
     }
 
-    @GetMapping("/export")
-    public AjaxJson export(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @PostMapping("/export")
+    public AjaxJson export(@RequestBody Schedule schedule, HttpServletRequest request, HttpServletResponse response) throws IOException {
         // 模拟查询数据
-        Schedule schedule = new Schedule();
-        schedule.setId(1);
-        schedule = service.get(schedule);
+        Schedule schedule1 = new Schedule();
+        schedule1.setId(1);
+        schedule1 = service.get(schedule1);
 
         ExportExcel excel = new ExportExcel();
         excel.createSheet("排班表");
         excel.setCellAlignment(HorizontalAlignment.CENTER, VerticalAlignment.CENTER);
-        excel.createRow().createCell(schedule.getDepartment().getName() + "日排班表").mergeCells(0, 0, 0, 8);
-        excel.createRow().createCell(schedule.getDate(), "yyyy年MM月dd日 EEEE");
-        excel.setCurrentCellIndex(8).createCell("800M：" + schedule.getM800());
+        excel.createRow().createCell(schedule1.getDepartment().getName() + "日排班表").mergeCells(0, 0, 0, 8);
+        excel.createRow().createCell(schedule1.getDate(), "yyyy年MM月dd日 EEEE");
+        excel.setCurrentCellIndex(8).createCell("800M：" + schedule1.getM800());
         excel.createRow().createCell("白班");
         excel.setCurrentCellIndex(2).createCell("领班", "班制", "工时", "人数", "人员", "应急分工");
 
