dd7949fecdc5f61735b4ae2e0675a486db11bf59
MCXCC <1270116382@qq.com>
2024-10-29 10:43:25 +0800

    feat(schedule): 添加排班数据导出功能
    - 新增 /export 接口，用于导出排班数据到 Excel 文件
    - 使用 Apache POI 库创建 XSSFWorkbook 对象
    - 设置响应头，实现文件下载
    - 目前仅创建空的 Excel 文件，未实现具体数据导出

--- src/main/java/org/szgb/console/schedule/web/ScheduleController.java
+++ src/main/java/org/szgb/console/schedule/web/ScheduleController.java
@@ -1,13 +1,24 @@
 package org.szgb.console.schedule.web;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.szgb.console.schedule.entity.Schedule;
 import org.szgb.console.schedule.mapper.ScheduleMapper;
 import org.szgb.console.schedule.service.ScheduleService;
 import org.szgb.core.base.web.BaseController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.szgb.core.utils.AjaxJson;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @RestController
@@ -17,5 +28,16 @@ public class ScheduleController extends BaseController<ScheduleService, Schedule
     @Autowired
     public ScheduleController(ScheduleService scheduleService) {
         super(scheduleService);
+    }
+
+    @GetMapping("/export")
+    public AjaxJson export(@RequestBody Schedule schedule, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        XSSFWorkbook wb = new XSSFWorkbook();
+        XSSFSheet sheet = wb.createSheet();
+        response.reset();
+        response.setContentType("application/octet-stream; charset=utf-8");
+        response.setHeader("Content-Disposition", URLEncoder.encode("fileName", StandardCharsets.UTF_8));
+        wb.write(response.getOutputStream());
+        return null;
     }
 }
