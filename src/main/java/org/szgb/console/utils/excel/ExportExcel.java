c4dc78ef5c164e2967b3c6fadbd713e8250cb18a
MCXCC <1270116382@qq.com>
2024-10-29 09:32:23 +0800

    feat(utils): 添加 Excel 导入导出工具类
    - 新增 ExportExcel 类，实现 Excel 文件导出功能
    - 新增 InputExcel 类，预留 Excel 文件导入功能- ExportExcel 类支持输出到数据流和客户端
    - 使用 Apache POI 库操作 Excel 文件

---
+++ src/main/java/org/szgb/console/utils/excel/ExportExcel.java
@@ -0,0 +1,37 @@
+package org.szgb.console.utils.excel;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class ExportExcel {
+    private Workbook wb = new XSSFWorkbook();
+
+    /**
+     * 输出数据流
+     *
+     * @param os 输出数据流
+     */
+    public ExportExcel write(OutputStream os) throws IOException {
+        wb.write(os);
+        return this;
+    }
+
+    /**
+     * 输出到客户端
+     *
+     * @param fileName 输出文件名
+     */
+    public ExportExcel write(HttpServletResponse response, String fileName) throws IOException {
+        response.reset();
+        response.setContentType("application/octet-stream; charset=utf-8");
+        response.setHeader("Content-Disposition", URLEncoder.encode(fileName, StandardCharsets.UTF_8));
+        write(response.getOutputStream());
+        return this;
+    }
+}
---
+++ src/main/java/org/szgb/console/utils/excel/InputExcel.java
@@ -0,0 +1,5 @@
+package org.szgb.console.utils.excel;
+
+public class InputExcel {
+
+}
