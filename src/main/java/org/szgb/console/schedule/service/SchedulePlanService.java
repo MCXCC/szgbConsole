4c5b152e1b78e04f9829bb5f30159b4ee0f6808d
chenlingzhi <1270116382@qq.com>
2025-01-15 15:55:48 +0800

    feat(schedule): 增加负责人和组员信息查询功能
    - 在 SchedulePlanMapper.xml 中添加了 responsible_person_id 和 group_member_ids 字段的查询
    - 在 SchedulePlanService 中实现了根据组员 ID 查询用户信息的方法
    - 优化了 findList 方法，为每个计划添加组员信息

--- src/main/java/org/szgb/console/schedule/service/SchedulePlanService.java
+++ src/main/java/org/szgb/console/schedule/service/SchedulePlanService.java
@@ -6,18 +6,29 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.szgb.console.schedule.entity.SchedulePlan;
 import org.szgb.console.schedule.mapper.SchedulePlanMapper;
+import org.szgb.console.sys.user.mapper.SysUserMapper;
 import org.szgb.core.base.service.BaseService;
 
 import java.util.List;
-import java.util.Objects;
 
 @Slf4j
 @Service
 public class SchedulePlanService extends BaseService<SchedulePlanMapper, SchedulePlan> {
+    private final SysUserMapper sysUserMapper;
 
     @Autowired
-    public SchedulePlanService(SchedulePlanMapper schedulePlanMapper) {
+    public SchedulePlanService(SchedulePlanMapper schedulePlanMapper, SysUserMapper sysUserMapper) {
         super(schedulePlanMapper);
+        this.sysUserMapper = sysUserMapper;
+    }
+
+    @Override
+    public List<SchedulePlan> findList(SchedulePlan entity) {
+        List<SchedulePlan> list = super.findList(entity);
+        list.forEach(schedulePlan -> {
+            schedulePlan.setGroupMember(sysUserMapper.getByIdList(schedulePlan.getGroupMemberIds().split(",")));
+        });
+        return list;
     }
 
     @Override
