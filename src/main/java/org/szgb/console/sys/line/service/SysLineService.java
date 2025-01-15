3010fc0db1beb2fa4750a887d0647114f356d5bf
chenlingzhi <1270116382@qq.com>
2025-01-15 14:28:50 +0800

    feat(sys): 添加线路树形结构接口
    - 在 SysLineController 中添加了 treeList 方法，用于获取线路树形结构数据
    - 在 SysLineService 中实现了 findTreeList 方法，用于查询线路树形结构数据
    - 新增了与 SysLineLocation 相关的 Mapper 和 Service

--- src/main/java/org/szgb/console/sys/line/service/SysLineService.java
+++ src/main/java/org/szgb/console/sys/line/service/SysLineService.java
@@ -3,16 +3,61 @@ package org.szgb.console.sys.line.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.szgb.console.sys.line.entity.SysLine;
+import org.szgb.console.sys.line.entity.SysLineLocation;
+import org.szgb.console.sys.line.mapper.SysLineLocationMapper;
 import org.szgb.console.sys.line.mapper.SysLineMapper;
 import org.szgb.core.base.service.BaseService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
 public class SysLineService extends BaseService<SysLineMapper, SysLine> {
 
+    private final SysLineLocationMapper sysLineLocationMapper;
+
     @Autowired
-    public SysLineService(SysLineMapper mapper) {
+    public SysLineService(SysLineMapper mapper, SysLineLocationMapper sysLineLocationMapper) {
         super(mapper);
+        this.sysLineLocationMapper = sysLineLocationMapper;
+    }
+
+    @Transactional(readOnly = true)
+    public List<SysLine> findTreeList() {
+        // 查询线路列表
+        List<SysLine> lineList = mapper.findList(new SysLine());
+
+        if (lineList == null || lineList.isEmpty()) {
+            return null;
+        }
+
+        // 查询站点列表
+        List<SysLineLocation> locationList = sysLineLocationMapper.findList(new SysLineLocation());
+
+        if (locationList == null || locationList.isEmpty()) {
+            return lineList;
+        }
+
+        // 插入站点列表到线路列表中
+        Map<Integer, List<SysLineLocation>> locationMap = new HashMap<>();
+        locationList.forEach(location -> {
+            try {
+                locationMap.get(location.getLine().getId()).add(location);
+            } catch (NullPointerException e) {
+                locationMap.put(location.getLine().getId(), new ArrayList<>());
+                locationMap.get(location.getLine().getId()).add(location);
+            }
+        });
+
+        lineList.forEach(line -> {
+            line.setLineLocationList(locationMap.get(line.getId()));
+        });
+
+        return lineList;
     }
 }
--- src/main/java/org/szgb/console/sys/line/web/SysLineController.java
+++ src/main/java/org/szgb/console/sys/line/web/SysLineController.java
@@ -1,13 +1,18 @@
 package org.szgb.console.sys.line.web;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.szgb.console.sys.line.entity.SysLine;
 import org.szgb.console.sys.line.mapper.SysLineMapper;
 import org.szgb.console.sys.line.service.SysLineService;
 import org.szgb.core.base.web.BaseController;
+import org.szgb.core.utils.AjaxJson;
 
 @Slf4j
 @RestController
@@ -16,5 +21,10 @@ public class SysLineController extends BaseController<SysLineService, SysLineMap
     @Autowired
     public SysLineController(SysLineService sysLineService) {
         super(sysLineService);
+    }
+
+    @PostMapping("/treeList")
+    public AjaxJson getTreeList(HttpServletRequest request, HttpServletResponse response) {
+        return AjaxJson.success(request, response).put("dictTree", service.findTreeList());
     }
 }
