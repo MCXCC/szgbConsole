8835d29f1a02e458d94998e15cfe144d7d6d12fa
凌志 陈 <1270116382@QQ.com>
2025-07-03 23:59:24 +0800

    refactor(sys-dict): 重构字典模块代码
    - 移除了 SysDictController 中的 treeList 方法
    - 优化了 SysDictMapper.xml 中的 SQL 查询
    - 删除了 SysDictService 中的 findTreeList 方法
    - 简化了代码结构，提高了可维护性

--- src/main/java/org/szgb/console/sys/dict/service/SysDictService.java
+++ src/main/java/org/szgb/console/sys/dict/service/SysDictService.java
@@ -7,13 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.szgb.console.sys.dict.entity.SysDict;
 import org.szgb.console.sys.dict.entity.SysDictChildren;
 import org.szgb.console.sys.dict.mapper.SysDictMapper;
-import org.szgb.console.sys.line.entity.SysLineLocation;
 import org.szgb.core.base.service.BaseService;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Service
@@ -26,42 +20,8 @@ public class SysDictService extends BaseService<SysDictMapper, SysDict> {
         this.sysDictChildrenService = sysDictChildrenService;
     }
 
-    @Transactional(readOnly = true)
-    public List<SysDict> findTreeList() {
-        // 查询父级字典列表
-        List<SysDict> dictList = mapper.findList(new SysDict());
-
-        // 查询子级字典列表
-        List<SysDictChildren> childrenList = sysDictChildrenService.findList(new SysDictChildren());
-
-        if (dictList == null || dictList.isEmpty()) {
-            return null;
-        }
-
-        if (childrenList == null || childrenList.isEmpty()) {
-            return dictList;
-        }
-
-        // 处理子级字典列表，将其与父级字典列表结合形成树形结构
-        Map<Integer, List<SysDictChildren>> childrenMap = new HashMap<>();
-        childrenList.forEach(children -> {
-            try {
-                childrenMap.get(children.getDict().getId()).add(children);
-            } catch (NullPointerException e) {
-                childrenMap.put(children.getDict().getId(), new ArrayList<>());
-                childrenMap.get(children.getDict().getId()).add(children);
-            }
-        });
-
-        dictList.forEach(dict -> {
-            dict.setChildren(childrenMap.get(dict.getId()));
-        });
-
-        return dictList;
-    }
-
     @Override
-    @Transactional(readOnly = false)
+    @Transactional()
     public void delete(String ids) {
         String[] id = ids.split(",");
         for (String s : id) {
--- src/main/java/org/szgb/console/sys/dict/web/SysDictController.java
+++ src/main/java/org/szgb/console/sys/dict/web/SysDictController.java
@@ -1,17 +1,13 @@
 package org.szgb.console.sys.dict.web;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.szgb.console.sys.dict.entity.SysDict;
 import org.szgb.console.sys.dict.mapper.SysDictMapper;
 import org.szgb.console.sys.dict.service.SysDictService;
-import org.szgb.core.utils.AjaxJson;
 import org.szgb.core.base.web.BaseController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -20,10 +16,5 @@ public class SysDictController extends BaseController<SysDictService, SysDictMap
     @Autowired
     public SysDictController(SysDictService sysDictService) {
         super(sysDictService);
-    }
-
-    @PostMapping("/treeList")
-    public AjaxJson getTreeList(HttpServletRequest request, HttpServletResponse response) {
-        return AjaxJson.success(request, response).put("dictTree", service.findTreeList());
     }
 }
