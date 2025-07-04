23fc5070004f52e8aadbf605a67780113bed6485
凌志 陈 <1270116382@QQ.com>
2025-07-04 22:38:16 +0800

    refactor(sys): 优化部门管理相关代码
    - 修改 SysDepartmentController 中 getDepartmentTree 方法的请求方式为 POST
    - 更新 SysDepartmentMapper.xml 中的 resultMap 配置，使用 result 标签替代 collection

--- src/main/java/org/szgb/console/sys/department/web/SysDepartmentController.java
+++ src/main/java/org/szgb/console/sys/department/web/SysDepartmentController.java
@@ -4,14 +4,14 @@ package org.szgb.console.sys.department.web;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.szgb.console.sys.department.entity.SysDepartment;
 import org.szgb.console.sys.department.mapper.SysDepartmentMapper;
 import org.szgb.console.sys.department.service.SysDepartmentService;
 import org.szgb.core.base.web.BaseController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.szgb.core.utils.AjaxJson;
 
 @Slf4j
@@ -24,7 +24,7 @@ public class SysDepartmentController extends BaseController<SysDepartmentService
         super(sysDepartmentService);
     }
 
-    @GetMapping("/getDepartmentTree")
+    @PostMapping("/getDepartmentTree")
     public AjaxJson getDepartmentTree(HttpServletRequest request, HttpServletResponse response) {
         return AjaxJson.success(request, response).put("data", service.getDepartmentTree());
     }
