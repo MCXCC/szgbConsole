3233dc92553b11f7784bea4232ebc217d0f8d723
凌志 陈 <1270116382@QQ.com>
2025-07-04 21:36:27 +0800

    refactor(console): 为 Page 类的分页相关字段添加 @JsonIgnore 注解
    - 在 Page 类中，为以下字段添加了 @JsonIgnore 注解：
      pageNo, pageSize, first, last, orderBy
    -这些字段通常仅用于后端分页逻辑，不需要在前端展示
    - 添加 @JsonIgnore 注解可以防止这些字段在序列化为 JSON 时被包含进去

--- src/main/java/org/szgb/console/utils/Page.java
+++ src/main/java/org/szgb/console/utils/Page.java
@@ -1,5 +1,6 @@
 package org.szgb.console.utils;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import java.io.Serial;
@@ -24,21 +25,25 @@ public class Page<T> implements Serializable {
     /**
      * 当前页码
      */
+    @JsonIgnore
     private Integer pageNo;
 
     /**
      * 每页显示的记录数
      */
+    @JsonIgnore
     private Integer pageSize;
 
     /**
      * 数据库查询的起始位置（通常用于分页查询）
      */
+    @JsonIgnore
     private Integer first;
 
     /**
      * 数据库查询的结束位置（通常用于分页查询）
      */
+    @JsonIgnore
     private Integer last;
 
     /**
@@ -49,5 +54,6 @@ public class Page<T> implements Serializable {
     /**
      * 排序条件
      */
+    @JsonIgnore
     private String orderBy;
 }
