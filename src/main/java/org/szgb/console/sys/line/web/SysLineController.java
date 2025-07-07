2ac3d7decfe219b7736843d67c4cba8803840ef6
凌志 陈 <1270116382@QQ.com>
2025-07-07 19:30:23 +0800

    feat(line): 增加线体和线体位置相关接口并优化数据结构
    - 新增 SysLineController 中的 findListForEquipment 方法
    - 在 SysLineLocation 中添加 equipmentList 属性
    - 新增 SysLineLocationEquipmentMapper 的 findListForTree 方法
    - 更新相关 XML 文件以支持新功能
    - 在 SysLineService 中添加 findListForEquipment 方法

--- src/main/java/org/szgb/console/sys/line/web/SysLineController.java
+++ src/main/java/org/szgb/console/sys/line/web/SysLineController.java
@@ -4,7 +4,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,13 +22,8 @@ public class SysLineController extends BaseController<SysLineService, SysLineMap
         super(sysLineService);
     }
 
-    @PostMapping("/treeList")
-    public AjaxJson getTreeList(HttpServletRequest request, HttpServletResponse response) {
-        return AjaxJson.success(request, response).put("lineTree", service.findTreeList());
-    }
-
     @PostMapping("/findListForEquipment")
-    public AjaxJson findListForEquipment(SysLine sysLine,HttpServletRequest request, HttpServletResponse response) {
+    public AjaxJson findListForEquipment(SysLine sysLine, HttpServletRequest request, HttpServletResponse response) {
         return AjaxJson.success(request, response).put("lineList", service.findListForEquipment(sysLine));
     }
 }
